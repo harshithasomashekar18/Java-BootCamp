08bda170a6fef30e7859576338cd23cb042b8fc4
harshithasomashekar18 <harshithasomashekar712@gmail.com>
2026-03-13 22:55:59 +0530

    Areaofrectangle

---
+++ com/day2/AreaOfRectangle.java
@@ -0,0 +1,10 @@
+package com.day2;
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int l = 20;
+        int b = 50;
+        int area = l*b;
+        System.out.println(area);
+    }
+    
+}
