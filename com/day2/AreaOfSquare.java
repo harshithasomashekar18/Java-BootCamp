27c846f751a21c133eabfc6eb536b4c3165a7260
harshithasomashekar18 <harshithasomashekar712@gmail.com>
2026-03-14 22:06:33 +0530

    p1

---
+++ com/day2/AreaOfSquare.java
@@ -0,0 +1,9 @@
+package com.day2;
+public class AreaOfSquare {
+
+    public static void main(String[] args) {
+        int l = 20;
+        int area = l*l*l*l;
+        System.out.println(area);
+    }
+}
