837bc1ebf0f64e94199d90b9daf5ea47dd4387fa
harshithasomashekar18 <harshithasomashekar712@gmail.com>
2026-03-15 22:32:19 +0530

    updated

--- com/day2/AreaOfRectangle.java
+++ com/day2/AreaOfRectangle.java
@@ -1,8 +1,12 @@
 package com.day2;
+import java.util.Scanner;
 public class AreaOfRectangle {
     public static void main(String[] args) {
-        int l = 20;
-        int b = 50;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length:");
+        int l = sc.nextInt();
+        System.out.println("Enter Breadth:");
+        int b = sc.nextInt();
         int area = l*b;
         System.out.println(area);
     }
