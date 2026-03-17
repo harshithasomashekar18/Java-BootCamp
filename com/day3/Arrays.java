652b2f7b20605da38fd18234c46cfd3adce77110
harshithasomashekar18 <harshithasomashekar712@gmail.com>
2026-03-17 22:25:00 +0530

    program

---
+++ com/day3/Arrays.java
@@ -0,0 +1,21 @@
+package com.day3;
+import java.util.Scanner;
+
+class Arrays {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int num = sc.nextInt();
+        if(num <= 1) {
+            System.out.println("Its neither prime nor composite!");
+        }
+        int c = 2;
+        while(c*c >= num){
+            if(num % c == 0){
+                System.out.println("Its a prime number");
+            }
+           c += 1; 
+        }
+        System.out.println("Its a composite number");
+    }
+}
