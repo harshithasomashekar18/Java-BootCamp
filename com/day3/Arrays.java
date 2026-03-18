e478152e18519c736eed2f6eaf8020c31e488637
harshithasomashekar18 <harshithasomashekar712@gmail.com>
2026-03-18 22:45:41 +0530

    update

--- com/day3/Arrays.java
+++ com/day3/Arrays.java
@@ -10,12 +10,15 @@ class Arrays {
             System.out.println("Its neither prime nor composite!");
         }
         int c = 2;
-        while(c*c >= num){
+        while(c*c <= num){
             if(num % c == 0){
-                System.out.println("Its a prime number");
+                System.out.println("Its a not prime number");
             }
            c += 1; 
+        
         }
-        System.out.println("Its a composite number");
+         System.out.println("Its a prime number");
+ 
+        }
+        
     }
-}
