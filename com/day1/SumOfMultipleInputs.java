3c3c2e87157c73b9ed237757315025a358431bb4
harshithasomashekar18 <harshithasomashekar712@gmail.com>
2026-03-20 19:55:34 +0530

    Take the input till user enter x and provide the sum of the input

---
+++ com/day1/SumOfMultipleInputs.java
@@ -0,0 +1,28 @@
+package com.day1;
+import java.util.Scanner;
+
+public class SumOfMultipleInputs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number (or 'x' to see the sum ):");
+        int total = 0;
+        while(true) {
+            if(sc.hasNextInt()) {
+                total += sc.nextInt();
+            }
+            else {
+                String input = sc.next();
+                if(input.equalsIgnoreCase("x")){
+                    break;
+                }
+                else {
+                    System.out.println("Invalid input. Enter number or X.");
+                }
+            }
+        }
+        System.out.println("Total Sum : " + total);
+        sc.close();
+    }
+
+    
+}
